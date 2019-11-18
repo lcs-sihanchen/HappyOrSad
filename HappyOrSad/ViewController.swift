0a78ba3f1cb2a27f9ddcf0ed43360abdf2a7986e
Chen Sihan <schen@lcs.on.ca>
2019-11-18 11:16:47 -0500

    Initial Commit

---
+++ HappyOrSad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HappyOrSad
+//
+//  Created by Chen, Sihan on 2019-11-18.
+//  Copyright © 2019 Chen, Sihan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
