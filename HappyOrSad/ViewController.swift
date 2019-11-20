36489f5c1055f36b7caa425e6b708b09bba3e492
Chen Sihan <schen@lcs.on.ca>
2019-11-20 00:05:35 -0500

    have it working

--- HappyOrSad/ViewController.swift
+++ HappyOrSad/ViewController.swift
@@ -30,14 +30,67 @@ class ViewController: UIViewController {
         if inputMessageAsString.count >= 255 {
             outputText.text = "Please enter a string with at least 1 and no more than 255 characters."
         }
+        var moodValue = 0
+        for scalars in inputMessageAsString.unicodeScalars {
+            let scalarValues = scalars.value
+            var scalarValuesAsString = String(scalarValues)
+            if scalarValuesAsString == "128522" {
+                moodValue += 1
+            }
+            if scalarValuesAsString == "128515" {
+                           moodValue += 1
+                       }
+            
+            if scalarValuesAsString == "9785" {
+                           moodValue -= 1
+                       }
+            if scalarValuesAsString == "128577" {
+                           moodValue -= 1
+                       }
+            
+            if scalarValuesAsString == "128533" {
+                                      moodValue -= 1
+                                  }
+            if scalarValuesAsString == "128532" {
+                                      moodValue -= 1
+                                  }
+//        if inputMessageAsString.contains("☹") {
+//            moodValue -= 1
+//        }
+//
+//        if inputMessageAsString.contains("🙁") {
+//            moodValue -= 1
+//        }
+//
+//        if inputMessageAsString.contains("😕") {
+//            moodValue -= 1
+//        }
+//        if inputMessageAsString.contains("😔") {
+//            moodValue -= 1
+//        }
+//
+//        if inputMessageAsString.contains("😃") {
+//            moodValue += 1
+//        }
+//
+//        if inputMessageAsString.contains("😊") {
+//            moodValue += 1
+//        }
+//        }
+        
+        if moodValue > 0 {
+            outputText.text = "Happy"
+        } else if moodValue < 0 {
+            outputText.text = "Sad"
+        } else {
+            outputText.text = "None"
+        }
+        //        😃 ,😊 ,
+        //      ☹ ,🙁 ,😕 ,😔
         
         
-//        😃 ,😊 ,
-//      ☹ ,🙁 ,😕 ,😔
-        
-        
+        }
     }
-    
     
     
     
